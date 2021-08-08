0f10e59c90bc0b529c2f9c4d65adfc54087322c3
eunhagfriend <48969007+santorini19970530@users.noreply.github.com>
2021-08-08 21:49:45 +0800

    Delete clock2.js

--- clock2.js
+++
@@ -1,8 +0,0 @@
-function keepTheClockMove()
-{
-	var n = new Date();
-	
-	document.getElementById("timePanel").innerHTML = (n.getHours()).toLocaleString('ja-jp', {minimumIntegerDigits: 2, useGrouping:false}) + ":" + (n.getMinutes()).toLocaleString('ja-jp', {minimumIntegerDigits: 2, useGrouping:false});
-	
-	setTimeout(keepTheClockMove, 1000);
-}
